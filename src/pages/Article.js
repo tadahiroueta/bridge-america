54e83870f00d58297aac490ba9d7968e1ab68e0f
tadahiroueta <tadahiroueta@gmail.com>
2023-05-12 10:06:34 -0500

    404

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -7,9 +7,10 @@ import { ArticleStructure, Markdown, Metadata } from '../components';
 export default function Article() {
     const [content, setContent] = useState({})
     
-    const { term } = useParams()
+    let { term } = useParams()
 
     const addLinks = (markdown, links) => {
+        // skip the title
         const lines = markdown.split('\n');
         const firstLine = lines[0];
         let rest = lines.slice(1).join('\n');
@@ -17,8 +18,7 @@ export default function Article() {
         links = links.filter(link => link !== term) // don't link to self
             .sort((a, b) => b.length - a.length); // check longer links first
 
-        for (const link of links) rest = rest.replace(
-            new RegExp(
+        for (const link of links) rest = rest.replace(new RegExp(
                 // replace - for ' ' and check that it's not already linked
                 `(?<!\\[|\\-|\\/)\\b${link.replace(/-/i, ' ')}\\b(?!\\]|\\-)`, 'gi'
             ), `[$&](/${link})`
@@ -27,19 +27,22 @@ export default function Article() {
     }
 
     useEffect(() => { 
-        (async () => {
+        const fetchContent = async term => {
             const user = await app.logIn(credentials);
             // get article and links in parallel
             Promise.all([ user.functions.getArticle(term), user.functions.getTerms() ])
                 // implement links and set content
                 .then(([ article, terms ]) => setContent({
                     ...article, markdown: addLinks(article.markdown, terms.terms)
-        }))})();
+                }))
+                .catch(() => fetchContent("404"))
+        }
+        fetchContent(term)
     }, [term])
 
     return (
         <ArticleStructure>
             <Markdown markdownText={ content.markdown } />
-            <Metadata author={ content.author } date={ content.date } />
+            { content.author && content.date ? <Metadata author={ content.author } date={ content.date } /> : null }
         </ArticleStructure>
 )}
