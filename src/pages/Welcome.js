eed1244081370caffa688abff7b527f90d7c35a3
tadahiroueta <tadahiroueta@gmail.com>
2023-05-16 09:29:37 -0500

    shuffle welcome before render

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,11 +7,15 @@ import { ListArticles } from "../components";
 export default function Welcome() {
   const [ markdown, setMarkdown ] = useState(" ");
 
-  const write = links => setMarkdown(addLinks(
-    "# Welcome to America\n\n### Here are some things you might want to know:\n\n" + 
-    links.sort((a, b) => .5 - Math.random()) // shuffle
-      .map(link => `* ${ link.replace(/-/g, ' ').toUpperCase() }`).join('\n')
-  , links))
+  const write = links => {
+    const content = addLinks(
+      "# Welcome to America\n\n### Here are some things you might want to know:\n\n" + 
+      links.sort((a, b) => .5 - Math.random()) // shuffle
+        .map(link => `* ${ link.replace(/-/g, ' ').toUpperCase() }`).join('\n')
+    , links);
+
+    setMarkdown(content)
+  }
 
   useEffect(() => { (async () => { 
     const user = await app.logIn(credentials);
